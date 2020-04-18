b5883fa1286a0a5dc83e00157397dd6f0ffa6169
Frederic Filosa <filosa@applistic.com>
2020-04-18 11:53:43 +0900

    First commit. Unity.

---
+++ Assets/Scripts/Common/GameController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // STATIC
+    // -------------------------------------------------------------------------
+
+    private static GameController s_instance;
+
+    // ACCESSORS
+    // -------------------------------------------------------------------------
+
+    public static GameController instance => s_instance;
+
+    // PRIVATE METHODS
+    // -------------------------------------------------------------------------
+    
+    private void Awake()
+    {
+        if (s_instance == null) {
+            s_instance = this;
+        }
+
+        if (this != s_instance) {
+            Destroy(gameObject);
+        }
+        
+        DontDestroyOnLoad(gameObject);
+    }
+}
